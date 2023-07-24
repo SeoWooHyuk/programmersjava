64aed261fbab64331c372b5c4b7aabde0e1c5cd6
SeoWooHyuk <tjdngur123@naver.com>
2023-07-24 16:52:18 +0900

    알고리즘공부

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Lv0 lv0 = new Lv0();
+        System.out.println( lv0.solution1(40) + " |나이출력|");
+        System.out.println( lv0.solution2(10,3) + " |양꼬치|");
+        System.out.println( lv0.solution3(5500)[0] + "," +  lv0.solution3(5500)[1] + " |아이스아메리카노|");
+        System.out.println( lv0.solution4(1234) + " |자리수더하기|");
+       
+    }
+}
